c49bf768845b9e06f219222da59124319a8e6664
Jesus David <jbernalme@gmail.com>
2021-02-03 15:15:41 -0500

    Depurando codigo

--- resources/views/components/widget-genres.blade.php
+++ resources/views/components/widget-genres.blade.php
@@ -1,7 +1,7 @@
 <div {!! $attributes->merge(['class' => 'flex flex-wrap justify-center']) !!}>
     @foreach ($genres as $id => $genre)
-        <span class="select-none cursor-pointer ml-2 mb-2 p-1 bg-gray-200 rounded-full" :class="{ 'bg-teal-400 text-white': selectedGenre === {{$id}} }" @click="selectedGenre = {{$id}}">{{ $genre }}</span>
-        @endforeach
+        <span class="select-none active:bg-teal-300 hover:bg-teal-400 hover:text-white cursor-pointer ml-2 mb-2 p-1 bg-gray-200 rounded-full" :class="{ 'bg-teal-400 text-white': selectedGenre.includes({{$id}}) }" @click="addGenres({{$id}})">{{ $genre }}</span>
+    @endforeach
 </div>
 
 
--- resources/views/series/home.blade.php
+++ resources/views/series/home.blade.php
@@ -19,26 +19,24 @@
                     <div class="grid grid-cols-4 gap-4 mt-6" x-data="main()" >
                         <div class=" col-span-4 lg:col-span-3">
                             <div class="flex justify-between flex-col sm:flex-row">
+                                @php
+                                    $navList = [
+                                        ['name' => 'Estrenos', 'href' => 'estrenos', 'option' => 'option-1'],
+                                        ['name' => 'Populares', 'href' => 'populares', 'option' => 'option-2'],
+                                        ['name' => 'Al Aire', 'href' => 'al-aire', 'option' => 'option-3'],
+                                        ['name' => 'Categorias', 'href' => 'categorias', 'option' => 'option-4'],
+                                    ]
+                                @endphp
                                 <ul class="flex text-base sm:text-lg">
-                                    <li class="p-2" >
-                                      <a class="select-none" :class="{ 'border-b-2 border-teal-400 text-gray-800 font-bold': selected === 'option-1' }" href="#option-1" @click="selected = 'option-1'">Estrenos</a>
-                                    </li>
-                                    <li class=" p-2">
-                                      <a class="select-none" :class="{ 'border-b-2 border-teal-400 text-gray-800 font-bold': selected === 'option-2' }" href="#option-2" @click="selected = 'option-2'">Populares</a>
-                                    </li>
-                                    <li class="p-2">
-                                      <a class="select-none" :class="{ 'border-b-2 border-teal-400 text-gray-800 font-bold': selected === 'option-3' }" href="#option-3" @click="selected = 'option-3'">Al Aire</a>
-                                    </li>
+                                    @foreach ($navList as $item)
+                                        <li class="p-2" >
+                                            <a class="select-none" :class="{ 'border-b-2 border-teal-400 text-gray-800 font-bold': selected === '{{$item['option']}}' }" href="/{{$item['href']}}" x-on:click.prevent @click="selected = '{{$item['option']}}', filter = false">{{$item['name']}}</a>
+                                        </li>
+                                    @endforeach
                                 </ul>
                                 <div class="relative">
-                                    <x-jet-input class="focus:outline-none border-none shadow-none leading-tight" placeholder="Buscar serie" x-model="searchValue" @keyUp.debounce.750="search()">
-
+                                    <x-jet-input class="" placeholder="Buscar serie" x-model="searchValue" @keyUp.debounce.750="search()">
                                     </x-jet-input>
-                                    <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
-                                        <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
-                                            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
-                                        </svg>
-                                    </button>
                                 </div>
 
                             </div>
@@ -51,40 +49,48 @@
                                 @foreach ($popularTv as $tvshow)
                                     <x-tv-card class="" :tvshow="$tvshow"/>
                                 @endforeach
-
                             </div>
                             <div class="mt-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4"  x-show.transition.in.opacity.duration.750ms="selected === 'option-3'">
                                 @foreach ($onAirTv as $tvshow)
                                     <x-tv-card class="" :tvshow="$tvshow"/>
                                 @endforeach
+                            </div>
+                            <div class="mt-8"  x-show.transition.in.opacity.duration.750ms="selected === 'option-4'">
+                                <h3 class="ml-4 mb-4">Filtrar Por Categoria:</h3>
+                                <x-widget-genres class="mb-4" :genres="$genres"></x-widget-genres>
+                                <x-search-results></x-search-results>
 
                             </div>
                             <div class="mt-8" x-show.transition.in.opacity.duration.750ms="selected === 'search'">
-                                <p class="inline text-xl font-bold text-gray-800 border-b-2 border-teal-400">Resultados de la busqueda: </p>
-                                <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
-                                    <template x-for="[id, value] in Object.entries(results)" :key="id">
-                                        <div>
-                                            <div class=" relative">
-                                                <a  href="">
-
-                                                    <img src="{{ Storage::url('sin-poster.png') }}" x-show="!value.poster_path" alt="poster" class="  hover:opacity-75 transition ease-in-out duration-150">
-                                                    <img x-bind:src="'https://www.themoviedb.org/t/p/w440_and_h660_face/' + value.poster_path" x-show="value.poster_path" alt="poster" class="  hover:opacity-75 transition ease-in-out duration-150">
-                                                    <span class="absolute bottom-2 right-2 px-2  bg-teal-500 rounded-full text-white text-sm" x-text="truncate(value.first_air_date, 0, 4)"></span>
-                                                </a>
-
-                                            </div>
-                                            <div class="mt-2">
-                                                <a href="" class="text-lg mt-2 hover:text-gray-300" x-text="value.name"></a>
-                                            </div>
+                                <x-search-results></x-search-results>
+                                <div class="flex flex-col items-center my-12">
+                                    <div class="flex text-gray-700">
+                                        <div class="h-8 w-8 mr-1 flex justify-center items-center rounded-full bg-gray-200 cursor-pointer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left w-4 h-4">
+                                                <polyline points="15 18 9 12 15 6"></polyline>
+                                            </svg>
                                         </div>
-                                    </template>
+                                        <div class="flex h-8 font-medium rounded-full bg-gray-200">
+                                            <div class="w-8 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full  ">1</div>
+                                            <div class="w-8 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full  ">...</div>
+                                            <div class="w-8 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full  ">3</div>
+                                            <div class="w-8 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full bg-pink-600 text-white ">4</div>
+                                            <div class="w-8 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full  ">5</div>
+                                            <div class="w-8 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full  ">...</div>
+                                            <div class="w-8 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full  ">15</div>
+                                            <div class="w-8 h-8 md:hidden flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in rounded-full bg-pink-600 text-white">4</div>
+                                        </div>
+                                        <div class="h-8 w-8 ml-1 flex justify-center items-center rounded-full bg-gray-200 cursor-pointer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right w-4 h-4">
+                                                <polyline points="9 18 15 12 9 6"></polyline>
+                                            </svg>
+                                        </div>
+                                    </div>
                                 </div>
-
                             </div>
                         </div>
 
                         <div class="hidden side-r lg:block">
-                            <x-widget-genres class=" mb-4" :genres="$genres"></x-widget-genres>
                             <x-widget-side :topRatedTv="$topRatedTv" take="5">
                                 <x-slot name="titulo">
                                     Mejor Calificadas
@@ -101,10 +107,51 @@
             return {
                 token: "{{config('services.tmdb.token')}}",
                 results: [],
-                prueba: 1,
-                selected: 'option-2',
-                selectedGenre: '',
+                filter: false,
+                selected: 'option-1',
+                selectedGenre: [],
+                stringGenre: '',
                 searchValue: '',
+                addGenres: function(id) {
+
+                    // Remueve el genero si este existe en el array o lo agrega al final del mismo
+                    if (this.selectedGenre.includes(id)) {
+                        const i = this.selectedGenre.indexOf(id);
+                        this.selectedGenre.splice(i, 1);
+                    } else {
+                        this.selectedGenre.push(id);
+                    }
+                    // convierte array a string separado por comas (,)
+                    this.stringGenre = this.selectedGenre.join();
+                    console.log(this.stringGenre);
+
+                    // Enviar peticion a axios
+                    const config = {
+                        headers: {
+                            authorization: 'Bearer ' + this.token
+                        },
+                        params: {
+                            language: 'es-mx',
+                            with_genres: this.stringGenre
+                        }
+                    };
+                    if (this.stringGenre) {
+                        axios
+                            .get('https://api.themoviedb.org/3/discover/tv', config)
+                            .then(respuesta => {
+
+                                this.results = respuesta.data.results;
+                                // var a = respuesta.data.results;
+                                console.log(respuesta.data.results);
+
+
+
+                            })
+                            .catch(error => console.log(error));
+                    }else{
+                        this.results = [];
+                    }
+                },
                 truncate: function(string, num1, num2) {
                     if (string) {
                         return string.slice(num1, num2);
@@ -127,6 +174,7 @@
 
                     if (this.searchValue) {
                         this.selected = "search";
+                        this.filter = false;
                         axios
                             .get('https://api.themoviedb.org/3/search/tv', config)
                             .then(respuesta => {
