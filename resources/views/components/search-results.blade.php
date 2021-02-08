cc64275dfddaf8065f96eacd142016b97dccdaca
Jesus David <jbernalme@gmail.com>
2021-02-08 09:47:53 -0500

    Fix pagination

--- resources/views/components/search-results.blade.php
+++ resources/views/components/search-results.blade.php
@@ -1,19 +1,19 @@
 <div>
     <p class="inline text-xl font-bold text-gray-800 border-b-2 border-teal-400">Resultados de la busqueda: </p>
-    <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
+    <div class="grid grid-cols-1 gap-4 mt-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         <template x-for="[id, value] in Object.entries(results)" :key="id">
             <div>
-                <div class=" relative">
+                <div class="relative ">
                     <a  href="">
 
-                        <img src="{{ Storage::url('sin-poster.png') }}" x-show="!value.poster_path" alt="poster" class="  hover:opacity-75 transition ease-in-out duration-150">
-                        <img x-bind:src="'https://www.themoviedb.org/t/p/w440_and_h660_face/' + value.poster_path" x-show="value.poster_path" alt="poster" class="  hover:opacity-75 transition ease-in-out duration-150">
-                        <span class="absolute bottom-2 right-2 px-2  bg-teal-500 rounded-full text-white text-sm" x-text="truncate(value.first_air_date, 0, 4)"></span>
+                        <img src="{{ Storage::url('sin-poster.png') }}" x-show="!value.poster_path" alt="poster" class="transition duration-150 ease-in-out hover:opacity-75">
+                        <img x-bind:src="'https://www.themoviedb.org/t/p/w440_and_h660_face/' + value.poster_path" x-show="value.poster_path" alt="poster" class="transition duration-150 ease-in-out hover:opacity-75">
+                        <span class="absolute px-2 text-sm text-white bg-teal-500 rounded-full bottom-2 right-2" x-text="truncate(value.first_air_date, 0, 4)"></span>
                     </a>
 
                 </div>
                 <div class="mt-2">
-                    <a href="" class="text-lg mt-2 hover:text-gray-300" x-text="value.name"></a>
+                    <a href="" class="mt-2 text-lg hover:text-gray-300" x-text="value.name"></a>
                 </div>
             </div>
         </template>
--- resources/views/series/home.blade.php
+++ resources/views/series/home.blade.php
@@ -16,9 +16,9 @@
                             <x-tv-card :tvshow="$tvshow" isslider="true"/>
                         @endforeach
                     </x-swiper>
-                    <div class="grid grid-cols-4 gap-4 mt-6" x-data="main()" >
+                    <div class="grid grid-cols-4 gap-4 mt-6" x-data="main()">
                         <div class="col-span-4 lg:col-span-3">
-                            <div class="flex flex-col justify-between sm:flex-row ">
+                            <div class="flex flex-col justify-between sm:flex-row" x-ref="topNav">
                                 @php
                                     $navList = [
                                         ['name' => 'Estrenos', 'href' => 'estrenos', 'option' => 'option-1'],
@@ -32,10 +32,7 @@
                                         <li class="p-2" >
                                             <a class="select-none" :class="{ 'border-b-2 border-teal-400 text-gray-800 font-bold': selected === '{{$item['option']}}' }" href="/{{$item['href']}}" x-on:click.prevent @click="selected = '{{$item['option']}}', filter = false">{{$item['name']}}</a>
                                         </li>
-                                        @endforeach
-                                        <li class="p-2" >
-                                            <a class="select-none"  x-on:click.prevent @click="pages()">prueba</a>
-                                        </li>
+                                    @endforeach
                                 </ul>
                                 <div class="relative">
                                     <x-jet-input class="" placeholder="Buscar serie" x-model="searchValue" @keyUp.debounce.750="search()">
@@ -68,34 +65,23 @@
                                 <x-search-results></x-search-results>
                                 <div class="flex flex-col items-center my-12">
                                     <div class="flex text-gray-700">
-                                        <button class="flex items-center justify-center w-8 h-8 mr-1 bg-gray-200 rounded-full cursor-pointer focus:outline-none active:bg-teal-300" @click="prevPage()">
+                                        <button class="flex items-center justify-center w-8 h-8 mr-1 bg-gray-200 rounded-full cursor-pointer focus:outline-none active:bg-teal-300" @click="prevPage($refs.topNav)">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 feather feather-chevron-left">
                                                 <polyline points="15 18 9 12 15 6"></polyline>
                                             </svg>
                                         </button>
                                         <div class="flex h-8 font-medium bg-gray-200 rounded-full select-none">
 
-
                                             <template x-for="(page, index) in array_pages">
-                                                <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " :class="{'text-white bg-pink-600' : current_page === array_pages[index] }" @click="current_page = array_pages[index]" x-text="array_pages[index]"></div>
+                                                <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " :class="{'text-white bg-pink-600' : current_page === array_pages[index] }" @click="current_page = array_pages[index], update_page()" x-text="array_pages[index]"></div>
                                             </template>
-
-
-                                            {{-- <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " :class="{'text-white bg-pink-600' : current_page === array_pages[0] }" @click="current_page = array_pages[0]" x-text="array_pages[0]"></div>
-                                            <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " :class="{'text-white bg-pink-600' : current_page === array_pages[1] }" @click="current_page = array_pages[1]" x-text="array_pages[1]"></div>
-                                            <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " :class="{'text-white bg-pink-600' : current_page === array_pages[2] }" @click="current_page = array_pages[2]" x-text="array_pages[2]"></div>
-                                            <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " :class="{'text-white bg-pink-600' : current_page === array_pages[3] }" @click="current_page = array_pages[3]" x-text="array_pages[3]"></div>
-                                            <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " :class="{'text-white bg-pink-600' : current_page === array_pages[4] }" @click="current_page = array_pages[4]" x-text="array_pages[4]"></div>
-                                            <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " :class="{'text-white bg-pink-600' : current_page === array_pages[5] }" @click="current_page = array_pages[5]" x-text="array_pages[5]"></div>
-                                            <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " :class="{'text-white bg-pink-600' : current_page === array_pages[6] }" @click="current_page = array_pages[6]" x-text="array_pages[6]"></div> --}}
-                                            {{-- <div class="items-center justify-center hidden w-8 leading-5 text-white transition duration-150 ease-in bg-pink-600 rounded-full cursor-pointer md:flex ">1</div> --}}
 
                                             <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex ">de</div>
                                             <div class="items-center justify-center hidden w-8 leading-5 transition duration-150 ease-in rounded-full cursor-pointer md:flex " x-text="total_pages"></div>
 
-                                            <div class="flex items-center justify-center w-8 h-8 leading-5 text-white transition duration-150 ease-in bg-pink-600 rounded-full cursor-pointer md:hidden">1</div>
+                                            <div class="flex items-center justify-center w-8 h-8 leading-5 text-white transition duration-150 ease-in bg-pink-600 rounded-full cursor-pointer md:hidden" x-text="current_page"></div>
                                         </div>
-                                        <button class="flex items-center justify-center w-8 h-8 ml-1 bg-gray-200 rounded-full cursor-pointer focus:outline-none active:bg-teal-300" @click="nextPage()">
+                                        <button class="flex items-center justify-center w-8 h-8 ml-1 bg-gray-200 rounded-full cursor-pointer focus:outline-none active:bg-teal-300" @click="nextPage($refs.topNav)">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 feather feather-chevron-right">
                                                 <polyline points="9 18 15 12 9 6"></polyline>
                                             </svg>
@@ -103,6 +89,7 @@
                                     </div>
                                 </div>
                             </div>
+                            {{-- <button @click="$refs.topNav.scrollIntoView()">subir</button> --}}
                         </div>
 
                         <div class="hidden side-r lg:block">
@@ -131,58 +118,91 @@
                 current_page: 1,
                 array_pages: [1],
                 pages: function(){
-
-                    this.current_page = 1;
+                    // Devulve un array de 7 o menos dependiendo del numero de paginas totales
+                    // this.current_page = 1;
                     if (this.total_pages <= 6) {
                         this.array_pages = Array.from({length: this.total_pages}, (v, i) => i + 1);
                     }else{
                         this.array_pages = Array.from({length: 7}, (v, i) => i + 1);
                     }
 
-                    // console.log('desde pages');
-                    // console.log(Object.values(this.array_pages));
-
                 },
-                prevPage: function(){
+                prevPage: function(a){
                     if (this.current_page > 1) {
                         this.current_page--;
+                        this.update_page();
                         console.log(this.current_page);
                         if (this.array_pages[0] > 1) {
                             this.array_pages = Array.from(this.array_pages, x => x - 1);
                         }
+                        this.update_page();
+                        setTimeout(function(){ a.scrollIntoView({ behavior: 'smooth' }); }, 500);
                     }
 
+
                 },
-                nextPage: function(){
+                nextPage: function(a){
                     if (this.current_page < this.total_pages) {
 
                         if (this.array_pages.indexOf(this.current_page) < 4) {
 
                             this.current_page++;
                             console.log('menor a 4 ', this.array_pages.indexOf(this.current_page));
-                            // console.log([1,2,3,4,5,6,7].indexOf(this.current_page));
 
                         }else{
-                            // // solo actializa el array si el ultimo item es distinto del total de paginas
+                            // solo actializa el array si el ultimo item es menor del total de paginas
                             if (this.array_pages[this.array_pages.length - 1] < this.total_pages ) {
-                                console.log('ultimo ', this.array_pages[this.array_pages.length - 1]);
+
                                 this.array_pages = Array.from(this.array_pages, x => x + 1);
                                 this.current_page++;
-                                // console.log(this.current_page);
 
                             }else{
                                 // si el vector ya llego a su fin (el ultimo item es = total_pages, incrementa current_page)
                                 this.current_page++;
                             }
                         }
+                        this.update_page();
+                        setTimeout(function(){ a.scrollIntoView({behavior: 'smooth' }); }, 500);
+                        // a.scrollIntoView({ behavior: 'smooth' });
+
                     }
+                },
+                update_page: function() {
+                    // Enviar peticion a axios
+                    const config = {
+                        headers: {
+                            authorization: 'Bearer ' + this.token
+                        },
+                        params: {
+                            language: 'es-mx',
+                            query: this.searchValue,
+                            page: this.current_page,
+                        }
+                    };
+
+                    if (this.searchValue) {
+                        this.selected = "search";
+                        axios
+                            .get('https://api.themoviedb.org/3/search/tv', config)
+                            .then(respuesta => {
+
+                                this.results = respuesta.data.results;
+                                this.total_pages = respuesta.data.total_pages;
+                                // var a = respuesta.data.results;
+                                // console.log(respuesta.data.results);
 
 
 
+                                // console.log(Array.from({
+                                //     length: this.total_pages,
+                                // }));
 
-                        // console.log(Object.values(this.array_pages));
-                        // this.array_pages = Array.from(this.array_pages, x => x + 1);
-                        // // console.log(this.current_page);
+                            })
+                            .catch(error => console.log(error));
+                    }else{
+                        this.results = [];
+                        this.selected = "option-1";
+                    }
 
                 },
                 addGenres: function(id) {
@@ -218,8 +238,6 @@
                                 this.total_pages = respuesta.data.total_pages;
                                 console.log(respuesta.data.total_pages);
 
-
-
                             })
                             .catch(error => console.log(error));
                     }else{
@@ -235,6 +253,8 @@
                     // console.log(event.target.value);
                     // console.log(this.searchValue);
 
+                    // Establecemos con pagina actual como la primera
+                    this.current_page = 1;
                     // Enviar peticion a axios
                     const config = {
                         headers: {
@@ -242,13 +262,12 @@
                         },
                         params: {
                             language: 'es-mx',
-                            query: this.searchValue
+                            query: this.searchValue,
                         }
                     };
 
                     if (this.searchValue) {
                         this.selected = "search";
-                        this.filter = false;
                         axios
                             .get('https://api.themoviedb.org/3/search/tv', config)
                             .then(respuesta => {
